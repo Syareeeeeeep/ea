6bbc6bd57b9d3be50c06e81663187fc3a9629aa8
ezrfst <ezar237644@student.smkn1kandeman.sch.id>
2025-06-12 12:58:02 +0700

    hari

--- index.php
+++ index.php
@@ -1,15 +1,103 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>hehe</title>
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="theme/plugins/fontawesome-free/css/all.min.css">
+    <!-- DataTables -->
+    <link rel="stylesheet" href="theme/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="theme/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="theme/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="theme/dist/css/adminlte.min.css">
     <style>
-        body{
-            background-color: blue;
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        .navigasi {
+            height: 100vh;
+            width: 35vh;
+            box-shadow: 0 5px 15px rgba(0, 0, 0, 0.35);
+        }
+
+        .navigasi .logo {
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            height: 10%;
+            width: 100%;
+            background-color: black;
+            color: white;
+            font-weight: 600;
+            font-size: 20px;
+        }
+
+        .logo img {
+            height: 60%;
+        }
+
+        .logout {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: 0.6s ease-in-out;
+            width: 100%;
+            text-decoration-line: none;
+            font-size: 20px;
+            font-weight: 600;
+            color: black;
+            padding: 10px;
+        }
+
+        .logout:hover {
+            padding: 20px 0;
+            background-color: rgb(221, 21, 21);
+            color: white;
         }
     </style>
 </head>
+
 <body>
+    <div class="container">
+        <div class="navigasi">
+            <div class="logo">
+                <img src="logo.png" alt="">
+                <p>Ezdeck</p>
+            </div>
+            <div class="">
+                <a href="regin.php" class="logout">Log Out</a>
+            </div>
+        </div>
+    </div>
+    <!-- jQuery -->
+    <script src="theme/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="theme/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- DataTables  & Plugins -->
+    <script src="theme/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="theme/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="theme/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="theme/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="theme/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="theme/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+    <script src="theme/plugins/jszip/jszip.min.js"></script>
+    <script src="theme/plugins/pdfmake/pdfmake.min.js"></script>
+    <script src="theme/plugins/pdfmake/vfs_fonts.js"></script>
+    <script src="theme/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+    <script src="theme/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+    <script src="theme/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="theme/dist/js/adminlte.min.js"></script>
 </body>
+
 </html>
--- regin.php
+++ regin.php
@@ -1,3 +1,44 @@
+<?php
+
+include("koneksi.php");
+session_start();
+
+$signup_message = '';
+
+if(isset($_POST["syarif"])){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql = "INSERT INTO user (username, password) VALUES ('$username', '$password')";
+
+    if($db->query($sql)){
+        $signup_message = "berhasil";
+        echo "berhasil";
+    }else {
+        $signup_message = "gagal";
+        echo "gagal";
+    }
+}
+
+
+if(isset($_POST["login"])) {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+    $sql = "SELECT * FROM user WHERE username='$username' AND password='$password'";
+    $result = $db->query($sql);
+    if($result->num_rows > 0) {
+        $data = $result->fetch_assoc();
+        $_SESSION["id"] = $data["id"];
+        $_SESSION["username"] = $data["username"];
+        $_SESSION["password"] = $data["password"];
+        $_SESSION["is_login"] = "users" ;
+        header("location: index.php");
+    }else{
+        echo "gagal";
+    }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,7 +52,7 @@
 <body>
     <div class="container" id="container">
         <div class="form-container sign-up">
-            <form action="signup.php" method="POST">
+            <form action="regin.php" method="POST">
                 <h1>Buat Akun</h1>
                 <label for="">Username</label>
                 <input type="text" name="username" placeholder="username" required>
@@ -21,7 +62,7 @@
             </form>
         </div>
         <div class="form-container sign-in">
-            <form action="login.php" method="POST">
+            <form action="regin.php" method="POST">
                 <h1>Login</h1>
                 <label for="">Username</label>
                 <input type="text" name="username" placeholder="username" required>
@@ -34,22 +75,19 @@
         <div class="toggle-container">
             <div class="toggle">
                 <div class="toggle-panel toggle-left">
-                    <h1>Welcome Back!</h1>
-                    <p>ganti untuk melihat fitur-fitur yang ada</p>
+                    <h1>Hello Pren!</h1>
+                    <p>Buat akun untuk mendaftarkan diri anda kedalam website</p>
                     <button class="hidden" id="login">Sign In</button>
                 </div>
-            </div>
-        </div>
-        <div class="toggle-container">
-            <div class="toggle">
                 <div class="toggle-panel toggle-right">
-                    <h1>Hello Pren!</h1>
+                    <h1>Welcome Back!</h1>
                     <p>Masuk untuk melihat fitur-fitur yang ada</p>
                     <button class="hidden" id="register">Sign Up</button>
                 </div>
             </div>
         </div>
-        <!-- <div class="toggle">
+    </div>
+    <!-- <div class="toggle">
                 <div class="toggle-container kiri">
                     <div class="divkiri">
                         <h1>Welcome Back!</h1>
